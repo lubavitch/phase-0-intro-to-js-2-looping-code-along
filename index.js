ea52414a75e947a4008cf4674a596cf316987d0e
mendel tenenbaum <mendelsadvertisements@hotmail.com>
2023-05-12 17:55:54 -0400

    while and for loops

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function writeCards(array, event){
+    
+    const messages = []
+for (let a = 0; a < array.length; a++)
+{console.log(`Thank you, ${array[a]}, for the wonderful ${event} gift!`)
+messages.push(`Thank you, ${array[a]}, for the wonderful ${event} gift!`)
+}
+return (messages)
+}
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+
+
+function countDown(number){
+while(number > -1){
+    //what we want to happen
+    console.log(number)
+    number--
+}
+}
+countDown(10)
